ca22298658ea6638e95eccf1aa269a4947f787e5
Jonas Hoffmann <s204071@dtu.dk>
2023-11-16 11:50:59 +0100

    fix

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,6 +6,8 @@
 #BSUB -J dl_sbert
 ### -- ask for number of cores (default: 1) --
 #BSUB -n 16
+#BSUB -gpu "num=1:mode=exclusive_process"
+
 ### -- specify that the cores must be on the same host --
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 4GB of memory per core/slot --
