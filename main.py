bb84ba9d76e34cd5a2814ae62b6f384914c7e06f
Jonas Hoffmann <s204071@dtu.dk>
2023-11-20 11:34:17 +0100

    add command line arguments

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ from sentence_transformers import SentenceTransformer
 from datamodule import MSMarcoDataModule
 from sbert_model import SBERT
 from lightning.pytorch.loggers import WandbLogger
+import click
 
 
-def train():
-    model = SentenceTransformer("bert-base-nli-mean-tokens")
+@click.command()
+@click.option("--batch_size", default=256, type=int)
+@click.option("--model", default="bert-base-nli-mean-tokens", type=str)
+@click.option("--epochs", default=50, type=int)
+@click.option("--seed", default=42, type=int)
+@click.option("--num_workers", default=None, type=int)
+@click.option("--lr", default=1e-5, type=float)
+def train(batch_size: int, model: str, epochs: int, seed: int, num_workers: int, lr: float):
+    L.seed_everything(seed)
+
+    model = SentenceTransformer(model)
 
     logger = WandbLogger(
         project="dl_sbert",
@@ -16,16 +26,16 @@ def train():
     )
 
     trainer = L.Trainer(
-        max_epochs=50,
+        max_epochs=epochs,
         accelerator="auto",
-        devices="auto",
+        devices=1 if torch.cuda.is_available() else "auto",
         deterministic=True,
         logger=logger
     )
 
-    datamodule = MSMarcoDataModule()
+    datamodule = MSMarcoDataModule(batch_size=batch_size, num_workers=num_workers)
 
-    l_module = SBERT(model, torch.nn.MSELoss())
+    l_module = SBERT(model, torch.nn.MSELoss(), lr=lr)
 
     trainer.fit(l_module, datamodule)
     trainer.test(l_module, datamodule)
