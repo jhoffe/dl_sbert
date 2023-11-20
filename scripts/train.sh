123b74095e4886ee52d10cf9a6fcd37b75635527
Jonas Hoffmann <s204071@dtu.dk>
2023-11-20 12:23:05 +0100

    fix

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ### General options
 ### -- specify queue --
-#BSUB -q gpuv100
+#BSUB -q gpua100
 ### -- set the job Name --
 #BSUB -J dl_sbert
 ### -- ask for number of cores (default: 1) --
