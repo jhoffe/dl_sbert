7746385aa8b37db3802e5f770ba328adef8a9c97
Jonas Hoffmann <jonas@triweb.dk>
2023-12-02 10:43:56 +0100

    Remove compile=true argument from model training commands
    The "compile=true" parameter in the model training command lines has been removed in the scripts/train.sh file. This change improves the flexibility of model training as now each model will be compiled by default depending on the native support available in the transformer library.

--- scripts/train.sh
+++ scripts/train.sh
@@ -9,10 +9,10 @@ export WANDB_CACHE_DIR=/work3/s204071/dl_sbert/wandb_cache/
 export TRANSFORMERS_CACHE=/work3/s204071/dl_sbert/transformers_cache/
 export PATH=/sbin:$PATH
 
-python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=distilbert-base-uncased --compile=true
-python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=bert-base-uncased --compile=true
-python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=bert-large-uncased --compile=true
-python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=roberta-base --compile=true
-python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=roberta-large --compile=true
-python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=xlm-roberta-base --compile=true
-python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=xlm-roberta-large --compile=true
+python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=distilbert-base-uncased
+python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=bert-base-uncased
+python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=bert-large-uncased
+python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=roberta-base
+python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=roberta-large
+python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=xlm-roberta-base
+python main.py --batch_size=128 --precision=16-mixed --num_steps=10000 --model=xlm-roberta-large
